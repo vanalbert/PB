8e355f8a64e920fb0049baca7af61f5c932a88e8
Van Albert <van@hexage.com>
2011-12-08 19:06:00 -0500

    Finished home and about pages.

---
+++ about-us-test.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<!-- Design by V. Albert/Hexage Designs 2010 -->
+<title>About Us Test Page For Drupal Installations</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="x-ua-compatible" content="IE=8" />
+<meta name="description" content="About Us Test Page For Drupal Installations" />
+<meta name="keywords" content="test />
+<meta name="author" content="Van Albert" />
+<link href="http://fonts.googleapis.com/css?family=IM+Fell+DW+Pica" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=Old+Standard" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=Droid Serif" rel="stylesheet" type="text/css" id="googlefont" />
+
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/html-reset.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/wireframes.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/layout-fixed.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/page-backgrounds.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/tabs.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/pages.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/blocks.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/navigation.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/views-styles.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/nodes.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/comments.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/forms.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/fields.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/default/files/fontyourface/font.css" />
+<link rel="stylesheet" type="text/css" media="print" href="sites/all/themes/vanzen/css/print.css" />
+
+
+
+</head>
+
+<body>
+<script type="text/javascript">
+   window.onload = function () {
+       Effect.Appear('content-fader', {duration: 1});
+   }
+</script>
+
+
+
+<div id="page-wrapper" class="clearfix">
+
+	<div id="page" class="clearfix">
+	
+		<div id="header">
+		<img src="http://local.prosperitybible.com/sites/all/themes/vanzen/logo.png" />
+		
+		</div><!-- #header close -->
+		
+		<div id="main-wrapper">
+			
+			<div id="main">
+
+				<div id="content" class="clearfix">
+
+						<div id="content-about" class="clearfix">
+							<div id="content-about-frame" class="clearfix">
+							<h2>About Us</h2>
+							<h3><span class="uline">Princeton Cambridge Publishing Group</span></h3>
+						
+							<div class="text-box">
+								<p class="about">The Wattles Prosperity Bible, The Law of Attraction Prosperity Bible and The Wattles Quotable Bible are published by Princeton Cambridge Publishing Group, Princeton, New Jersey.</p>
+
+								<p class="about">Princeton Cambridge Publishing Group is the work product of Richard A. Catalina, Jr., Esq., the company’s President and CEO.  In addition to ProsperityBible.com, Princeton Cambridge Publishing Group owns and operates the Success and Personal Development web site, <a target="_blank" href="http://www.successmanual.com/" title="The Success Manual - FREE Success Ebooks - FREE Self Improvement Ebooks - Success Keys">The Success Manual</a>, at <a href="http://www.successmanual.com/">www.successmanual.com</a>.  The Success Manual is a feature-packed site, offering a<a target="_blank" href="http://www.successmanual.com/category/success-manual-downloads/" title="The Success Manual FREE Ebook Download Library Post Category - FREE Success Ebooks for Immediate Download. Published by Princeton Cambridge Publishing Group, Princeton, New Jersey, United States of America.  &copy; Copyright 2010 The Princeton Licensing Group, Princeton, New Jersey, United States of America.  All Rights Reserved. Enjoy!!"> VIRTUAL LIBRARY of FREE</a> high-quality Success Ebooks (55 FREE Ebooks at last count) for immediate download, <a target="_blank" href="http://www.successmanual.com/category/success-manual-success-keys/" title="The Success Manual SUCCESS KEYS Page: SUCCESS KEYS are concise, proven and POWERFUL Success Principles, Strategies and Mastery Moves for you to implement into your daily mental programming. SUCCESS KEYS are the fundamentals, and as you work these SUCCESS KEYS into your Thought processes, you will begin to bring about the necessary internal transformation to attain SUCCESS CONSCIOUSNESS. SUCCESS KEYS are directly based on the teachings and works of the great Masters of Success; in order to achieve Success, we must think like and model ourselves after Successful people.">Success Keys</a> and <a target="_blank" href="http://www.successmanual.com/category/success-strategist-manual-articles-laws-of-success/" title="The Success Manual SUCCESS STRATEGIES Page: The Success Strategist Manual consists of a series of thought-provoking Success based inquiries into our thinking processes. We must engage in consistent critical analyses of the Thoughts that we harbor within. Our Thoughts are the fundamental building blocks of our belief systems, conditioning, habits and paradigms. These, in turn, determine our behavior, within a true &#34;cause and effect&#34; system. Our behavior, in turn, determines our actions; and our actions, in turn, determine our results. Our results, the effects, are what we may see and perceive through our intellectual faculties, but it is our Thoughts that are the cause. Original essays will be posted on a consistent basis in the Success Strategist Manual. Read them. Study them. Understand them. Use them. Increase your level of awareness by bringing forth Growth through the Science and Art of Purposeful Thought.">Success Strategies</a> &mdash; content rich articles and posts providing valuable information crucial to attaining Health, Wealth, Prosperity and Success &mdash; and <a target="_blank" href="http://www.successmanual.com/success-beacon-free-newsletter/" title="FREE Newsletter: The SUCCESS BEACON! - Subscribe NOW. The Success Beacon! is The Success Manual's FREE informative newsletter that will provide you with compelling insights, teachings and strategies developed by the great Success Masters, Teachers and Sages that will literally TRANSFORM your " circumstance-controlled,="circumstance-controlled," "victim"=""victim"" failure="failure" consciousness="consciousness" into="into" Powerful,="Powerful," Creative="Creative" and="and" Masterful="Masterful" SUCCESS="SUCCESS" CONSCIOUSNESS!"="CONSCIOUSNESS!"">The Success Beacon newsletter</a>, an informative of material to assist your journey to be, do and have more in this Life.</p>
+								<p class="about">Richard is a practicing Intellectual Property, Technology and Internet law attorney with offices in Robbinsville and Princeton, New Jersey, and New York, New York.  He has been practicing law for more than twenty years and has been named a “New Jersey Super Lawyer” for seven consecutive years (2004-2010) appearing in annual editions of <em>New Jersey</em> monthly magazine.</p>
+							</div><!-- .text-box close -->
+							
+							<div class="text-box">
+								<p align="center"><strong><span style="text-decoration: underline;">Richard A. Catalina, Jr.</span></strong></p>
+								<p class="about">Richard is a life-long Student of Success and has been studying the Secrets of Success since his college days when he participated in The est Seminar Training program and became a student of yoga and meditation.  Since then, Richard has read hundreds of books on the subjects of self improvement, personal development and Success and studied materials from Anthony Robbins to Zig Ziglar.  During 2008-2009, Richard studied as a personal student under one of the greatest Success Masters and Thinkers of our modern day, Bob Proctor, and Richard's present Life philosophy as to the impact of Thought (cause) as the originator of the events and circumstances of our lives (effect) heavily reflects the teachings of Bob Proctor.</p>
+								<p class="about">Richard's favorite Success books include <em>Think and Grow Rich</em> by Napoleon Hill, <em>The Greatest Salesman in the World</em> by Og Mandino, <em>Awaken the Giant Within</em> by Anthony Robbins, <em>Secrets of the Millionaire Mind</em> by T. Harv Eker, <em>The Secret of the Ages</em> by Robert Collier, <em>As a Man Thinketh</em> by James Allen and <em>The Science of Getting Rich</em> by Wallace D. Wattles.</p>
+
+								<p class="about">In December 2009, Richard launched his own Success website,<strong> <a href="http://www.successmanual.com/">The Success Manual</a></strong>, and its companion free weekly newsletter, <strong>The Success Beacon</strong>, both of which are published by Princeton Cambridge Publishing Group.  The Success Manual features original articles, essays and other Success based references and offers<strong> <a href="http://www.successmanual.com/category/success-manual-downloads/">FREE quality Ebooks</a></strong> of history's greatest Success classics.</p>
+								<p class="about">In July 2010, Richard launched his first product series, <strong>The Wattles Prosperity Bible</strong>, <strong>The Law of Attraction Prosperity Bible</strong> and <strong>The Wattles Quotable Bible</strong>, all published by Princeton Cambridge Publishing Group and available at <strong><a href="http://www.prosperitybible.com/">www.prosperitybible.com</a></strong>.</p>
+
+								<p class="about">Richard is married and is a devoted husband and father to three beautiful children.  When not working, Richard enjoys spending time with his family.</p>
+								<p class="about"><strong>Follow Richard on Twitter: @SuccessBeacon </strong></p>
+							
+							</div><!-- .text-box close -->
+							
+
+
+
+							
+
+
+
+
+								<div id="divider"> 
+									<img src="http://local.prosperitybible.com/sites/all/themes/vanzen/images/divider.png" /> 
+								</div><!-- #divider close -->	
+							</div>							
+						</div><!--  #content-about close -->
+
+	</div><!-- #content close-->
+   </div><!-- #main close -->
+	</div><!-- #main-wrapper close -->
+   </div><!-- #page close -->
+   </div><!-- #page-wrapper close -->
+
+
+
+
+</body>
+</html>
---
+++ home-test.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<!-- Design by V. Albert/Hexage Designs 2010 -->
+<title>Home Test Page For Drupal Installations</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="x-ua-compatible" content="IE=8" />
+<meta name="description" content="Home Test Page For Drupal Installations" />
+<meta name="keywords" content="test />
+<meta name="author" content="Van Albert" />
+<link href="http://fonts.googleapis.com/css?family=IM+Fell+DW+Pica" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=Old+Standard" rel="stylesheet" type="text/css" id="googlefont" />
+<link href="http://fonts.googleapis.com/css?family=Droid Serif" rel="stylesheet" type="text/css" id="googlefont" />
+
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/html-reset.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/wireframes.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/layout-fixed.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/page-backgrounds.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/tabs.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/pages.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/blocks.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/navigation.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/views-styles.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/nodes.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/comments.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/forms.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/all/themes/vanzen/css/fields.css" />
+<link rel="stylesheet" type="text/css" media="screen,projection" href="sites/default/files/fontyourface/font.css" />
+<link rel="stylesheet" type="text/css" media="print" href="sites/all/themes/vanzen/css/print.css" />
+
+
+
+</head>
+
+<body>
+<script type="text/javascript">
+   window.onload = function () {
+       Effect.Appear('content-fader', {duration: 1});
+   }
+</script>
+
+
+
+<div id="page-wrapper" class="clearfix">
+
+	<div id="page" class="clearfix">
+	
+		<div id="header">
+		<img src="http://local.prosperitybible.com/sites/all/themes/vanzen/logo.png" />
+		
+		</div><!-- #header close -->
+		
+		<div id="main-wrapper">
+			
+			<div id="main">
+
+				<div id="content" class="clearfix">
+
+						<div class="clearfix" id="content-intro">
+
+						<h2 class="intro-one">Welcome to the World&#39;s Greatest Success, Prosperity and Law of Attraction Library!</h2>
+
+						<h2 class="intro-two">FINALLY - In a SINGLE Collection - The COMPLETE Works of Wallace D. Wattles - World Master of the Law of Attraction - and ALL of the Works from &quot;THE SECRET&quot;</h2>
+						<p class="what-if"><strong>What If? </strong>What if <strong><span style="text-decoration: underline;">YOU </span></strong>Could Have Access to the World&#39;s Greatest Wisdom on Achieving Success, Attaining Prosperity, Acquiring Wealth and Living a Life of Health, Wellness and Energy?</p>
+
+						</div>
+						<!-- #content-intro close -->
+						
+						
+						<div class="clearfix" id="content-body-wrap">
+						<div id="content-body">
+							<div class="box-white">
+								<p class="now-you-can"><span class="uline">NOW YOU CAN</span>&mdash; with <br/>THE SUCCESS MANUAL PROSPERITY LIBRARY!</p>
+								<div id="content-body-grouplarge">
+									<img alt="Success Manual Prosperity Bible - The Timeless Works Of Wallace Wattles" height="420" src="http://local.prosperitybible.com/sites/all/themes/vanzen/images/grouplarge.jpg" title="Success Manual Prosperity Bible - The Timeless Works Of Wallace Wattles" width="550" />
+								</div><!-- #content-grouplarge close -->
+								
+							</div><!-- .box-white close -->
+							
+							<dl><dd>
+							<div style="text-align: center;"><span style="font-family: book antiqua,palatino;"><span style="font-size: 12pt;"><span style="color: rgb(128, 0, 0);"><strong><span style="font-size: 14pt;">41 EBooks of Timeless&nbsp;Spiritual Knowledge.</span></strong></span></span></span></div>
+							</dd><dd>
+							<div style="text-align: center;"><span style="color: rgb(128, 0, 0);"><strong><span style="font-family: book antiqua,palatino;"><span style="font-size: 14pt;">5,400 Pages of Secret Wisdom to Living the Life You Desire and Deserve.</span></span></strong></span></div>
+							</dd><dd>
+							<div style="text-align: center;"><span style="color: rgb(128, 0, 0);"><strong><span style="font-family: book antiqua,palatino;"><span style="font-size: 14pt;">REVEALED: Lost KEYS to Health, Wealth, Prosperity and Success.</span></span></strong></span></div>
+							</dd><dd>
+							<div style="text-align: center;"><span style="color: rgb(128, 0, 0);"><strong><span style="font-family: book antiqua,palatino;"><span style="font-size: 14pt;">Profound Insight into Universal Truths of YOUR &quot;SELF.&quot;</span></span></strong></span></div>
+							</dd><dd>
+							<div style="text-align: center;"><span style="color: rgb(128, 0, 0);"><strong><span style="font-family: book antiqua,palatino;"><span style="font-size: 14pt;">The ORIGINAL TEACHINGS on The Law of Attraction.</span></span></strong></span></div>
+							</dd><dd><span style="font-size: 14pt;">&nbsp;</span></dd><dd>
+							<div style="text-align: center;"><span style="font-family: book antiqua,palatino;"><span style="font-size: 12pt;"><strong><span style="font-size: 14pt;"><span style="color: rgb(128, 0, 0);"><span style="font-size: 18pt;">Interested? Well, <span class="highlight">READ ON . . .</span></span></span></span></strong></span></span></div>
+							</dd></dl>
+							
+							
+							
+							
+							<div class="box-white">
+								<div id="inline-image"><img alt="The Wattles Prosperity Bible Volume 1" class="inline-left" height="359" src="http://local.prosperitybible.com/sites/all/themes/vanzen/images/home-img2.jpg" title="Success Manual Prosperity Bible - The Timeless Works Of Wallace Wattles" width="275" />
+								<p class="regular"><span class="drop-cap">T</span>he book and movie that have intrigued the world over, are only part of the story. &ldquo;<em>The Secret</em>&rdquo; by Rhonda Byrne, re-introduced the &ldquo;Law of Attraction&rdquo; into popular culture, but few have ventured beyond into the actual teachings and principles upon which <em>The Secret</em> is based. Although centuries old, these teachings and principles were rigorously investigated and developed in the early 1900s and are deeply rooted and founded upon the philosophy and works of a little-known author named Wallace Delois Wattles and his book, <em>The Science of Getting Rich</em>. The fact is that while many works and teachings are cited in <em>The Secret</em>, Wattles&rsquo; book perhaps the most profound and most compelling inspiration for Rhonda Byrne&rsquo;s <em>The Secret</em>.&nbsp; <strong>How compelling</strong>?&nbsp; When Rhonda first read <em>The Science of Getting Rich</em>, she was so moved by Wattle&rsquo;s teachings that she cried!</p>	
+								</div><!-- #inline-image close -->
+							</div><!-- .box-white close -->
+							
+							<div class="box-bg">
+								<p class="alert"><strong>Imagine - the movie and&nbsp;book that&nbsp;inspired the world were&nbsp;inspired&nbsp;by Teachings, Principles&nbsp;and Universal Laws&nbsp;that you are now about to discover!</strong></p>	
+								<p class="regular"><strong><span class="red-text">The Wattles Prosperity Bible</span></strong> is a compilation of works compiled into an incredible, <strong>POWERFUL FIVE&nbsp;(5) VOLUME SET</strong> - <span class="highlight"><strong>COMPRISING MORE THAN 1,400 PAGES</strong></span> - that includes <strong>ALL</strong> of the books, essays, articles and works by Mr. Wattles that delve deeply into the underlying scientific teachings and principles of the Universal Laws &ndash; including the Law of Attraction &ndash; only superficially covered in <em>The Secret</em>.</p>
+								<p class="regular"><strong>AND, as a <span class="red-highlight"><span style="text-decoration: underline;">Limited Time Bonus</span></span>, with your purchase of <span class="red-text">The Wattles Prosperity Bible</span>, you&nbsp; will also receive - <span class="highlight"><span style="text-decoration: underline;">FREE</span></span> - <span class="red-text">The Law of Attraction Prosperity Bible: </span><span class="highlight">A Five-Volume Compilation of the World&#39;s Greatest Law of Attraction Works, comprised of 18 COMPLETE EBooks and more than 4,000 PAGES! Every work mentioned in the book, <em>The Secret,</em> and MUCH MORE!</span></strong></p>
+								<p class="regular"><span class="red-text"><strong>The Wattles Prosperity Bible</strong></span> will transform your understanding of the Law of Attraction &ndash; and the many other Universal Laws that govern <strong>YOUR LIFE</strong> &nbsp;&ndash; from the &ldquo;mystical&rdquo; to the practical, from the intangible to the tangible, from the theoretical to the real!</p>
+								<p class="regular">These Universal Laws are <strong>NOT</strong> magic, &ldquo;hocus-pocus,&rdquo; or other vague, esoteric gibberish without practical meaning or application.&nbsp; These Universal Laws are sound, scientific and practical laws that govern <strong>YOUR</strong> Life and <strong>YOUR</strong> World. &nbsp;You can either work <em>WITH</em> the Laws . . . or you can choose to work <em>AGAINST</em> the Laws . . . <strong>which would you choose?</strong></p>
+								<p class="regular">What most DON&rsquo;T understand or otherwise FAIL to realize is that the Law of Attraction is a Science, and, as such, it requires real, practical and tangible efforts by the practitioner.&nbsp; <span class="highlight">If you want to attain real, honest and lasting Health, Wealth, Prosperity and Success, MORE is required than merely entertaining your Desires &ndash; <strong>You MUST take ACTION according to the Laws of the Science!</span></strong></p>
+								<p class="regular"><strong>While others sit idly by waiting for fortune to come to them, with <span class="red-text">The Wattles Prosperity Bible</span>, <span class="highlight">YOU will be among the very few who will possess ALL of the Keys to Success written by Wattles</span>. With this comprehensive Success manual as YOUR PERSONAL GUIDE, you will be among the very few who will have access to the Science on manifesting your Desires and getting the most out of life by taking the proper, requisite Actions that yield<span class="highlight"> REAL Results</span>.</strong><span id="_marker">&nbsp;</span></p>
+							</div>
+
+							
+
+							
+							
+
+							
+							<div id="divider"> 
+								<img src="http://local.prosperitybible.com/sites/all/themes/vanzen/images/divider.png" /> 
+							</div><!-- #divider close -->						
+						</div><!--  #content-body close -->
+
+	</div><!-- #content close-->
+   
+</div><!-- #page-container close -->
+
+</body>
+</html>
